d3f5f2e0b089c03d80ee4c48b8d9bd76b62da31a
janik6882 <>
2021-02-07 19:12:36 +0100

    added docu

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ import json
 
 class Wrapper():
     def __init__(self):
+        """
+        Comment: standard init
+        Input: name of instanze
+        Output: Nothing
+        Special: Nothing
+        """
         self.base = "https://europe-west3-brdata-corona.cloudfunctions.net/lglApi/"
-        
