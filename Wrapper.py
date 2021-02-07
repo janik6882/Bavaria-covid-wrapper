ab7d14fdb6a63d6306851f7b7d0d3bc045be8cea
janik6882 <janik@klauenberg.eu>
2021-02-07 19:51:57 +0100

    added documentaton

--- Wrapper.py
+++ Wrapper.py
@@ -12,26 +12,46 @@ class Wrapper():
         self.base = "https://europe-west3-brdata-corona.cloudfunctions.net/lglApi/"
 
     def get_data(self):
-        # TODO: add docu
+        """
+        Comment: gets all data
+        Input: Name of Instance
+        Output: All Data available
+        Special: Json data might be quite a bit
+        """
         url = self.base
         r = requests.get(url)
         return json.loads(r.content)
 
     def get_current(self):
-        # TODO: add docu
+        """
+        Comment: gets the last available data
+        Input: Name of Instance
+        Output: All available current data (only for the current day)
+        Special: Nothing Special
+        """
         url = self.base + "date"
         r = requests.get(url)
         return json.loads(r.content)
 
     def get_data_date(self, date):
-        # TODO: add dpcu
+        """
+        Comment: gets all data for a certain day
+        Input: Name of instance, date
+        Output: Data as Json
+        Special: Date format: Year-Month-Day
+        """
         temp_url = self.base + "date/{date}"
         url = temp_url.format(date=date)
         r = requests.get(url)
         return json.loads(r.content)
 
     def get_data_county(self, county_id=None):
-        # TODO: add docu
+        """
+        Comment: get all data for a certain county, without county all data
+        Input: Name of Instance, optional: county_id
+        Output: Server Response as Json
+        Special: If no county_id is given, all data will be returned
+        """
         if county_id:
             temp_url = self.base + "county/{county_id}"
             url = temp_url.format(county_id=county_id)
